ef0ae4e103af16c47d1b55135617a7d00ee0dda6
mathiaslyhr <mathiaslyhroutlook.com>
2025-09-12 11:46:50 +0200

    kontakt færdig

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Projects from './components/Projects.jsx'
-import Skills from './components/Skills.jsx'
 import Contact from './components/Contact.jsx'
 import Footer from './components/Footer.jsx'
 
